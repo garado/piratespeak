f8d9d9b0bde1d3583b1c9166af3d012636a42e9d
Alexis Garado <alexisgarado@gmail.com>
2023-02-04 18:24:48 -0800

    basic skeleton for... stuff

---
+++ js/convert.js
@@ -0,0 +1,7 @@
+
+function updateOutputText() {
+    const input_text  = document.getElementById('input_text');
+    const output_text = document.getElementById('output_text');
+
+    output_text.innerHTML = input_text.value;
+}
